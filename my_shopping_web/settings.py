61cf92c05da5af6ec9b98dad8de647f7b0d8cd7a
TirthDhanani <tirthdhanani0243@gmail.com>
2025-08-18 14:57:30 +0530

    18-09-2025

--- my_shopping_web/settings.py
+++ my_shopping_web/settings.py
@@ -157,3 +157,5 @@ EMAIL_HOST_PASSWORD = "ukvvhccqchgjilqz"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MYURL=""
